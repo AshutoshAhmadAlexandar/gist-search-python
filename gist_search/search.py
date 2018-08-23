cc6d6e6c000e337a6e48117f813b97f246d31329
Ashutosh Ahmad Alexandar <ashutoshahmadalexandar@Ashutoshs-MBP.fios-router.home>
2018-08-22 23:03:42 -0400

    Commit

---
+++ gist_search/search.py
@@ -0,0 +1,52 @@
+from gist_search.utils import get_gists
+
+
+def search_gists(username, description=None, file_name=None):
+    if not description and not file_name:
+        print("At least one search parameter must be specified")
+        return
+    
+    gists = get_gists(username)
+    if gists is None:
+        print("Request failed")
+        return
+    if gists == []:
+        print("Couldn't find gists for user {}".format(username))
+        return
+    
+    results = []
+    for gist in gists:
+        if description and description.lower() not in gist['description'].lower():
+            continue
+        if file_name:
+            matched = False
+            for json_fname in gist['files']:
+                if file_name in json_fname:
+                    matched = True
+                    break
+            if not matched:
+                continue
+        results.append(gist)
+    return results
+        
+        
+        
+        
+
+        
+        # if description and file_name:
+        #     if description and description.lower() in gist['description'].lower():
+        #         for json_fname in gist['files']:
+        #             if file_name in json_fname:
+        #                 results.append(gist)
+        # elif description and description.lower() in gist['description'].lower():
+        #     results.append(gist)
+        # elif file_name:
+        #     for json_fname in gist['files']:
+        #         if file_name in json_fname:
+        #             results.append(gist)
+                    
+
+    
+    
+    
---
+++ gist_search/utils.py
@@ -0,0 +1,17 @@
+import requests
+
+BASE_URL = 'https://api.github.com/users/{username}/gists'
+
+
+def get_gists(username):
+    url = BASE_URL.format(username=username)
+    resp = requests.get(url, params={'per_page': 100})
+    if not resp.ok:
+        return None
+    return resp.json()
+
+if __name__ == '__main__':
+    gists = get_gists('gvanrossum')
+    first_three_gists = gists[:3]
+    for gist in first_three_gists:
+        print("{:<40} | {}".format(gist['id'], gist['description']))
